20c3c16851902f01dc1b09eebb32626859337083
Rohit Menon <rmenon@lyft.com>
2018-10-05 09:58:48 -0700

    Fix build - import order

--- gateway/src/main/java/com/lyft/data/gateway/handler/SparkGatewayRoutingHandler.java
+++ gateway/src/main/java/com/lyft/data/gateway/handler/SparkGatewayRoutingHandler.java
@@ -3,14 +3,18 @@ package com.lyft.data.gateway.handler;
 import com.lyft.data.gateway.config.ProxyBackendConfiguration;
 import com.lyft.data.gateway.router.GatewayBackendManager;
 import com.lyft.data.proxyserver.ProxyHandler;
+
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.apache.http.HttpHeaders;
 import org.eclipse.jetty.client.api.Request;
 import org.eclipse.jetty.http.HttpHeader;
 
-import javax.servlet.http.HttpServletRequest;
-import java.util.List;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
+
 
 public class SparkGatewayRoutingHandler extends ProxyHandler {
   final GatewayBackendManager gatewayBackendManager;
