b76cd2c0a042a6338c5e42b330ec29b890faf268
James Taylor <jamestaylor@lyft.com>
2020-04-10 17:00:46 -0700

    Update /v1/cluster path to /ui/api/stats path using old path as fallback

--- gateway-ha/src/main/java/com/lyft/data/gateway/ha/handler/QueryIdCachingProxyHandler.java
+++ gateway-ha/src/main/java/com/lyft/data/gateway/ha/handler/QueryIdCachingProxyHandler.java
@@ -28,7 +28,8 @@ public class QueryIdCachingProxyHandler extends ProxyHandler {
   public static final String V1_STATEMENT_PATH = "/v1/statement";
   public static final String V1_QUERY_PATH = "/v1/query";
   public static final String V1_INFO_PATH = "/v1/info";
-  public static final String V1_CLUSTER_PATH = "/v1/cluster";
+  public static final String V1_CLUSTER_PATH = "/v1/cluster"; // Deprecated
+  public static final String V1_CLUSTER_STATS_PATH = "/ui/api/stats";
   public static final String PRESTO_UI_PATH = "/ui";
   public static final String USER_HEADER = "X-Presto-User";
   public static final String SOURCE_HEADER = "X-Presto-Source";
@@ -80,7 +81,8 @@ public class QueryIdCachingProxyHandler extends ProxyHandler {
         || path.startsWith(V1_QUERY_PATH)
         || path.startsWith(PRESTO_UI_PATH)
         || path.startsWith(V1_INFO_PATH)
-        || path.startsWith(V1_CLUSTER_PATH);
+        || path.startsWith(V1_CLUSTER_PATH)
+        || path.startsWith(V1_CLUSTER_STATS_PATH);
   }
 
   public boolean isAuthEnabled() {
