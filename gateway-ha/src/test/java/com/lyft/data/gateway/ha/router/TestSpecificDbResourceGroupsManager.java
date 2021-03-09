27618fb9848cf8f93b620bded7f8f038f297eaf2
Ritesh Varyani <riteshvaryani@lyft.com>
2021-03-08 22:35:27 -0800

    checkstyle violation fix

--- gateway-ha/src/test/java/com/lyft/data/gateway/ha/router/TestSpecificDbResourceGroupsManager.java
+++ gateway-ha/src/test/java/com/lyft/data/gateway/ha/router/TestSpecificDbResourceGroupsManager.java
@@ -28,8 +28,8 @@ public class TestSpecificDbResourceGroupsManager extends TestResourceGroupsManag
     String jdbcUrl = "jdbc:h2:" + tempH2DbDir.getAbsolutePath();
     HaGatewayTestUtils.seedRequiredData(
             new HaGatewayTestUtils.TestConfig("", tempH2DbDir.getAbsolutePath()));
-    DataStoreConfiguration db = new DataStoreConfiguration(jdbcUrl, "sa"
-            , "sa", "org.h2.Driver");
+    DataStoreConfiguration db = new DataStoreConfiguration(jdbcUrl, "sa",
+            "sa", "org.h2.Driver");
     JdbcConnectionManager connectionManager = new JdbcConnectionManager(db);
     super.resourceGroupManager = new HaResourceGroupsManager(connectionManager);
   }
