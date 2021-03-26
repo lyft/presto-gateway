1a57e59e6d80672145cae81deb2baa935ec67fb1
Nilesh Kumar <nsonic1994@gmail.com>
2021-03-26 20:40:42 +0530

    config and entrypoint added

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,5 +8,10 @@ mkdir -p /etc/presto-gateway
 
 curl -s https://consul.moengage.com/v1/kv/dw-presto/presto-gateway/gateway-ha-config.yml | jq -r '.[] | .Value' | base64 --decode > /etc/presto-gateway/gateway-ha-config.yml
 
-java -jar /app/gateway-ha-jar-with-dependencies.jar server /etc/presto-gateway/gateway-ha-config.yml
+echo "Fetching bintray password"
+sql_user=$(aws ssm get-parameter --name "/presto/presto-gateway/sql_username" | grep "Value" | awk -F\" '{print $4}')
+sql_password=$(aws ssm get-parameter --name "/presto/presto-gateway/sql_password" --with-decryption | grep "Value" | awk -F\" '{print $4}')
 
+sed -i -e "s/\${SQL_USERNAME}/$sql_user/" -e "s/\${SQL_PASSWORD}/$sql_password/" /etc/presto-gateway/gateway-ha-config.yml
+
+java -jar /app/gateway-ha-jar-with-dependencies.jar server /etc/presto-gateway/gateway-ha-config.yml
