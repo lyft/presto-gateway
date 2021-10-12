5fba41cfe333e42dcbd0fd92672c8b9befb7b1a7
Christopher He <christopherhe@lyft.com>
2021-10-12 14:41:48 -0700

    add unit test

--- gateway-ha/src/test/java/com/lyft/data/gateway/ha/router/TestRoutingGroupSelector.java
+++ gateway-ha/src/test/java/com/lyft/data/gateway/ha/router/TestRoutingGroupSelector.java
@@ -4,12 +4,21 @@ import static com.lyft.data.gateway.ha.router.RoutingGroupSelector.ROUTING_GROUP
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import java.io.FileReader;
 import javax.servlet.http.HttpServletRequest;
+import org.jeasy.rules.api.Rule;
+import org.jeasy.rules.api.Rules;
+import org.jeasy.rules.core.RuleBuilder;
+import org.jeasy.rules.mvel.MVELRuleFactory;
+import org.jeasy.rules.support.reader.JsonRuleDefinitionReader;
+import org.jeasy.rules.support.reader.YamlRuleDefinitionReader;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 @Test
 public class TestRoutingGroupSelector {
+  public static final String TRINO_SOURCE_HEADER = "X-Trino-Source";
+
   public void testByRoutingGroupHeader() {
     HttpServletRequest mockRequest = mock(HttpServletRequest.class);
 
@@ -21,5 +30,25 @@ public class TestRoutingGroupSelector {
     // If the header is not present just return null.
     when(mockRequest.getHeader(ROUTING_GROUP_HEADER)).thenReturn(null);
     Assert.assertNull(RoutingGroupSelector.byRoutingGroupHeader().findRoutingGroup(mockRequest));
+  }
+
+  public void testByRoutingRulesEngine() {
+    HttpServletRequest mockRequest = mock(HttpServletRequest.class);
+
+    when(mockRequest.getHeader(TRINO_SOURCE_HEADER)).thenReturn("airflow");
+
+    Rule airflowRule = new RuleBuilder()
+        .name("airflow rule")
+        .description("if airflow route to etl cluster")
+        .when(facts -> ((HttpServletRequest) facts.get("request")).getHeader(
+              TRINO_SOURCE_HEADER).equals("airflow"))
+        .then(facts -> facts.put("routingGroup", "etl"))
+        .build();
+
+    Rules rules = new Rules(airflowRule);
+    Assert.assertEquals(
+        RoutingGroupSelector.byRoutingRulesEngine(rules).findRoutingGroup(mockRequest),
+        "etl");
+
   }
 }
